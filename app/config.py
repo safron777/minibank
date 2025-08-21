0d9a5e03229d250cb95398609b379fe0f92b4a89
Alex Safronov <safronov_alexey@mail.ru>
2025-08-21 17:30:42 +0300

    Revert "autotest"
    This reverts commit dafe77cc70ebbd4d2f701e2fa27243e5f86115d2.
    
    Signed-off-by: Alex Safronov <safronov_alexey@mail.ru>

--- app/config.py
+++ app/config.py
@@ -5,8 +5,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    MODE: Literal["DEV", "TEST", "PROD"]
-    LOG_LEVEL: str
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
@@ -16,13 +14,6 @@ class Settings(BaseSettings):
     @property
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-    
-    
-    TEST_DB_HOST: str
-    TEST_DB_PORT: int
-    TEST_DB_USER: str
-    TEST_DB_PASS: str
-    TEST_DB_NAME: str
 
 
     SECRET_KEY: str
@@ -30,22 +21,5 @@ class Settings(BaseSettings):
     # Со 2 версии Pydantic, class Config был заменен на атрибут model_config
     # class Config:
     #     env_file = ".env"
-    model_config = SettingsConfigDict(env_file=".env-non-dev")
-
-
-    @property
-    def TEST_DATABASE_URL(self):
-        return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
-    
-
-   # REDIS_HOST: str
-    #REDIS_PORT: int
-
-    #SENTRY_DSN: str
-
-    SECRET_KEY: str
-    ALGORITHM: str
-
     model_config = SettingsConfigDict(env_file=".env")
-
 settings = Settings()
--- app/feature_toggle/models.py
+++ app/feature_toggle/models.py
@@ -4,7 +4,7 @@ from app.database import Base
 #from app.users import models
 #from app.users.models import Users
 
-class Feature_toggle(Base):
+class feature_toggle(Base):
     __tablename__="feature_toggle"
     id = Column(Integer,primary_key=True)
     feature_toggle=Column(BOOLEAN,nullable=False)
